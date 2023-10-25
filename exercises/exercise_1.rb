63877aac25b8d903d9660b1cba2619f0ec96da39
Devvaru <syeraru@gmail.com>
2023-10-25 21:47:54 +0200

    Create and count 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+Store.create(:name => 'Burnaby', :mens_apparel => true, :womens_apparel => true, :annual_revenue => 300000)
+
+Store.create(:name => 'Richmond', :mens_apparel => false, :womens_apparel => true, :annual_revenue => 1260000)
+
+Store.create(:name => 'Gastown', :mens_apparel => false, :womens_apparel => false, :annual_revenue => 190000)
+
+puts Store.count
