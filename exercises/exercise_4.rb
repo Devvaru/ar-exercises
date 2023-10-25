51798501fe048d0e4baae6d35f056349665205d3
Devvaru <syeraru@gmail.com>
2023-10-25 23:25:41 +0200

    Load a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(:name => 'Surrey', :mens_apparel => false, :womens_apparel => true, :annual_revenue => 224000)
+
+Store.create(:name => 'Whistler', :mens_apparel => true, :womens_apparel => false, :annual_revenue => 1900000)
+
+Store.create(:name => 'Yaletown', :mens_apparel => true, :womens_apparel => true, :annual_revenue => 430000)
+
+@mens_stores = Store.where(:mens_apparel => true, :womens_apparel => false).load
+
+@mens_stores.all.each do |store|
+    puts "Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(:mens_apparel => false, :womens_apparel => true).where( 'annual_revenue < ?', 1000000).load
+
+# This way doesn't work
+# @womens_stores = Store.where(mens_apparel: false, womens_apparel: true, 'annual_revenue < 1000000').load
+
+
