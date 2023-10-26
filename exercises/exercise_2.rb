6469133679881e6eff996cbc8ce0bc20ef114984
Devvaru <syeraru@gmail.com>
2023-10-26 19:20:42 +0200

    Fix variable assignment

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,7 +6,7 @@ puts "----------"
 
 # Your code goes here ...
 
-@store1 = Store.where(:id => 1).load
-@store2 = Store.where(:id => 2).load
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
 
 @store1.update(:name => 1)
